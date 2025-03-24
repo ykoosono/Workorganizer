94baaa117f394450748fb6dc716d0b2a67e5fbe5
clacombe <clacombe@CompSciICv-10.fsc.int>
2025-03-24 11:44:12 -0400

    Worked on signup

--- signupAction.php
+++ signupAction.php
@@ -2,12 +2,13 @@
 
 require "DBConnect.php";
 
+
 $user = $_GET["user"];
 $pswd = $_GET["pswd"];
 $email = $_GET["email"];
 
-$sql = "insert into user values (0, '" . $user . "', '" . $pswd . "', '" .
-  $email . "')";
+$sql = "insert into users values (0, '" . $user . "', '" . $email . "', '" .
+  $pswd . "', '" . "a" . "')";
 
 echo modifyDB($sql) . "<br>Use back button to return";
 
