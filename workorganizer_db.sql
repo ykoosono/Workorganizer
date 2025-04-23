72acaa9afa11c8363072408b26b6610db3b043a9
caseylacombeFSU <clacombe@student.fitchburgstate.edu>
2025-04-23 11:48:46 -0400

    Add files via upload

--- workorganizer_db.sql
+++ workorganizer_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 03, 2025 at 06:17 PM
+-- Generation Time: Apr 23, 2025 at 05:47 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -29,9 +29,51 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `calendar` (
   `id` int(11) NOT NULL,
-  `start_date` datetime NOT NULL,
-  `end_date` datetime NOT NULL
+  `title` varchar(100) NOT NULL,
+  `month` varchar(11) NOT NULL,
+  `year` year(4) NOT NULL,
+  `1st` varchar(255) NOT NULL,
+  `2nd` varchar(255) NOT NULL,
+  `3rd` varchar(255) NOT NULL,
+  `4th` varchar(255) NOT NULL,
+  `5th` varchar(255) NOT NULL,
+  `6th` varchar(255) NOT NULL,
+  `7th` varchar(255) NOT NULL,
+  `8th` varchar(255) NOT NULL,
+  `9th` varchar(255) NOT NULL,
+  `10th` varchar(255) NOT NULL,
+  `11th` varchar(255) NOT NULL,
+  `12th` varchar(255) NOT NULL,
+  `13th` varchar(255) NOT NULL,
+  `14th` varchar(255) NOT NULL,
+  `15th` varchar(255) NOT NULL,
+  `16th` varchar(255) NOT NULL,
+  `17th` varchar(255) NOT NULL,
+  `18th` varchar(255) NOT NULL,
+  `19th` varchar(255) NOT NULL,
+  `20th` varchar(255) NOT NULL,
+  `21st` varchar(255) NOT NULL,
+  `22nd` varchar(255) NOT NULL,
+  `23rd` varchar(255) NOT NULL,
+  `24th` varchar(255) NOT NULL,
+  `25th` varchar(255) NOT NULL,
+  `26th` varchar(255) NOT NULL,
+  `27th` varchar(255) NOT NULL,
+  `28th` varchar(255) NOT NULL,
+  `29th` varchar(255) NOT NULL,
+  `30th` varchar(255) NOT NULL,
+  `31st` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `calendar`
+--
+
+INSERT INTO `calendar` (`id`, `title`, `month`, `year`, `1st`, `2nd`, `3rd`, `4th`, `5th`, `6th`, `7th`, `8th`, `9th`, `10th`, `11th`, `12th`, `13th`, `14th`, `15th`, `16th`, `17th`, `18th`, `19th`, `20th`, `21st`, `22nd`, `23rd`, `24th`, `25th`, `26th`, `27th`, `28th`, `29th`, `30th`, `31st`) VALUES
+(1, 'Test Calend', 'March', '2025', '1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th', '11th', '12th', '13th', '14th', '15th', '16th', '17th', '18th', '19th', '20th', '21st', '22nd', '23rd', '24th', '25th', '26th', '27th', '28th', '29th', '30th', '31st'),
+(2, 'Test Calendar', 'February', '2025', '1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th', '11th', '12th', '13th', '14th', '15th', '16th', '17th', '18th', '19th', '20th', '21st', '22nd', '23rd', '24th', '25th', '26th', '27th', '28th', 'non-applicable', 'non-applicable', 'non-applicable'),
+(3, 'Test Calendar', 'February', '2025', '1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th', '11th', '12th', '13th', '14th', '15th', '16th', '17th', '18th', '19th', '20th', '21st', '22nd', '23rd', '24th', '25th', '26th', '27th', '28th', '', '', ''),
+(4, 'Test Calendar 2', 'April', '2025', '1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th', '11th', '12th', '13th', '14th', '15th', '16th', '17th', '18th', '19th', '20th', '21st', '22nd', '23rd', '24th', '25th', '26th', '27th', '28th', '29th', '30th', 'non-applicable');
 
 -- --------------------------------------------------------
 
@@ -111,8 +153,18 @@ CREATE TABLE `users` (
   `name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `created_at` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `name`, `email`, `password`, `created_at`) VALUES
+(1, 'clacombe', 'clacombe@fitchburg.edu', 'clacombe', '0000-00-00 00:00:00'),
+(14, 'calacombe', 'calacombe@fitchburg.edu', 'clacombe', '2025-03-26 02:10:36'),
+(16, 'mahadev', 'abc@123.com', 'clacombe', '2025-03-26 15:50:12'),
+(18, 'alacombe', 'clacombe@fitchburgstate.edu', 'clacombe', '2025-04-07 15:17:53');
 
 -- --------------------------------------------------------
 
@@ -163,7 +215,9 @@ ALTER TABLE `role_permissions`
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`);
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `name` (`name`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- Indexes for table `users_calendars`
@@ -182,7 +236,7 @@ ALTER TABLE `users_calendars`
 -- AUTO_INCREMENT for table `calendar`
 --
 ALTER TABLE `calendar`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `permissions`
@@ -206,7 +260,7 @@ ALTER TABLE `role_permissions`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `users_calendars`
